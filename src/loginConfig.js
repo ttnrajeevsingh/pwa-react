0313b1dc7613df8fafb82a2317dc3a56c06f68e6
ttnrajeevsingh <69036284+ttnrajeevsingh@users.noreply.github.com>
2021-12-14 16:00:04 +0530

    Update loginConfig.js

--- src/loginConfig.js
+++ src/loginConfig.js
@@ -1,6 +1,6 @@
 let loginConfig = {
   "publicKey": {
-    "challenge": "LdeUWFohT0EUZmRv/LbnlAHmitrMRcCvjIJcYPh9QLk=",
+    "challenge": "2AA7iXFAJJRJMJMcRc3bl9R5u68hLQexVkKt9q3wqyA=",
     "timeout": 60000,
     "rpId": "main--objective-shannon-3368bd.netlify.app",
     "allowCredentials": [
@@ -13,7 +13,54 @@ let loginConfig = {
     "extensions": {
       "txAuthSimple": ""
     },
-    "userVerification": "discouraged"
+    "userVerification": "discouraged",
+    "pubKeyCredParams": [
+      {
+        "type": "public-key",
+        "alg": -7
+      },
+      {
+        "type": "public-key",
+        "alg": -35
+      },
+      {
+        "type": "public-key",
+        "alg": -36
+      },
+      {
+        "type": "public-key",
+        "alg": -257
+      },
+      {
+        "type": "public-key",
+        "alg": -258
+      },
+      {
+        "type": "public-key",
+        "alg": -259
+      },
+      {
+        "type": "public-key",
+        "alg": -37
+      },
+      {
+        "type": "public-key",
+        "alg": -38
+      },
+      {
+        "type": "public-key",
+        "alg": -39
+      },
+      {
+        "type": "public-key",
+        "alg": -8
+      }
+    ],
+    "authenticatorSelection": {
+      "requireResidentKey": false,
+      "userVerification": "discouraged"
+    },   
+    "attestation": "none"
   }
 }
 export {loginConfig};
